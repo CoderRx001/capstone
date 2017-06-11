8dade0096cf00e9aee3c7ab1ab8ef8b7a159827f
CoderRx001 <carrillo964@gmail.com>
2017-06-11 16:59:41 -0500

    update 1

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -5,6 +5,18 @@ class FoodsController < ApplicationController
     sort_attribute = params[:sort]
     sort_order = params[:sort_order]
     discount = params[:discount]
+    search_term = params[:search_term]
+    category =params[:category]
+
+    if category
+      @foods = Category.find_by(name: category).popcorns
+    end
+
+    if sort_attribute && sort_order
+      @products = @products.order(sort_attribute => sort_order)
+    elsif sort_attribute
+    @products = @products.order(sort_attribute)
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   #
 
   get '/signup' => 'users#new'
-  post '/ussers' => 'users#create'
+  post '/users' => 'users#create'
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
